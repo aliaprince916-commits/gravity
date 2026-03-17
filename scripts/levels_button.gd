3143bacdd9c843c7232078e0d2eaf257b56582a5
aliaprince916-commits <aliaprince916@gmail.com>
2026-03-17 13:43:55 +0100

    intialze

--- scripts/levels_button.gd
+++ scripts/levels_button.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _on_level_one_pressed() -> void:
 	if  Gm.is_open("res://scenes/lvl_prototype.tscn"):
-		Transitionlayer.change_scean("res://scenes/lvl_prototype.tscn")
+		Transition.change_scean("res://scenes/lvl_prototype.tscn")
 
 
 

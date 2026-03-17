1a7e7698f376fcef4771975237c9f1ade5a0c581
aliaprince916-commits <aliaprince916@gmail.com>
2026-03-17 14:29:05 +0100

    ربي يعيشك ارتاح

--- scripts/levels_button.gd
+++ scripts/levels_button.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 func _on_level_one_pressed() -> void:
 	if  Gm.is_open("res://scenes/lvl_prototype.tscn"):
-		Transition.change_scean("res://scenes/lvl_prototype.tscn")
+		Transition.change_scean("res://scenes/lvl_prototype.tscn",1)
 		
 func _on_level_one_2_pressed() -> void:
 	if  Gm.is_open("res://scenes/lvl.tscn"):
-		Transition.change_scean("res://scenes/lvl.tscn")
+		Transition.change_scean("res://scenes/lvl.tscn",2)

2ea0266b2f65d0294ba7df85bb01b65e33e1e9bc
aliaprince916-commits <aliaprince916@gmail.com>
2026-03-17 17:53:46 +0100

    روح ارقد

--- scripts/levels_button.gd
+++ scripts/levels_button.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+# دالة تغيير المشهد + التحقق
+func _on_pressed(lvl, num):
+	if Gm.is_open(lvl):
+		Transition.change_scean(lvl, num)
 
-func _on_level_one_pressed() -> void:
-	if  Gm.is_open("res://scenes/lvl_prototype.tscn"):
-		Transition.change_scean("res://scenes/lvl_prototype.tscn",1)
-		
-func _on_level_one_2_pressed() -> void:
-	if  Gm.is_open("res://scenes/lvl.tscn"):
-		Transition.change_scean("res://scenes/lvl.tscn",2)
+func _on_level_2_pressed() -> void:
+	_on_pressed("res://scenes/lvl.tscn",2)
+
+
+func _on_level_1_pressed() -> void:
+	_on_pressed("res://scenes/lvl_prototype.tscn",1)

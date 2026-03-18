0b5a9c991dbfd0bda75478e9ba1b30ef7f36df3d
wadiehamad2010-oss <wadie.hamad.2010@gmail.com>
2026-03-18 12:33:48 +0100

    sounds

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 
+@onready var click: AudioStreamPlayer2D = $click
+
 func _on_continue_pressed() -> void:
+	click.play()
 	Transition.change_scean("res://scenes/lvl_prototype.tscn",1,"")
 func _on_play_pressed() -> void:
+	click.play()
 	Transition.change_scean("res://scenes/main_lvl.tscn",0,"levels")
 func _on_quit_pressed() -> void:
 	get_tree().quit()

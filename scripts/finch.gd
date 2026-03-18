57830f88fa1906d2577b28dc7ff13f32ef47d6c6
wadiehamad2010-oss <wadie.hamad.2010@gmail.com>
2026-03-18 13:12:58 +0100

    menu

--- scripts/finch.gd
+++ scripts/finch.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 # تحديد المسار والرقم من الـ Inspector
 @export_file("*.tscn") var target_level: String
-@export var level_number: int = 2
+@export var level_number: int = 1
 
 func _on_body_entered(body: Node2D) -> void:
-	# التحقق من أن الكائن هو اللاعب
+
 	if body.name == "player" or body.is_in_group("player"):
 		if target_level != "":
 			# استدعاء دالة الانتقال
--- scripts/levels_button.gd
+++ scripts/levels_button.gd
@@ -7,10 +7,11 @@ func _on_pressed(lvl, num):
 	if Gm.is_open(lvl):
 		Transition.change_scean(lvl, num,"")
 
-func _on_level_2_pressed() -> void:
-	_on_pressed("res://scenes/lvl.tscn",2)
+
 func _on_level_1_pressed() -> void:
 	_on_pressed("res://scenes/lvl_prototype.tscn",1)
+func _on_level_2_pressed() -> void:
+	_on_pressed("res://scenes/lvl.tscn",2)
 
 func _on_home_pressed() -> void:
 	click.play()
